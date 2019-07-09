5d082659696e2ebf9d1e30fc2f613c0ddfaf6676
illusi0n <miloshb92@hotmail.com>
2019-07-09 23:05:21 +0200

    First iteration of Builder Pattern

---
+++ src/patterns/creational/builder/LunchOrderBean.java
@@ -0,0 +1,50 @@
+package patterns.creational.builder;
+
+/**
+ * Not immutable.
+ * Not sure what the contract is regarding which parameter is required and which is optional.
+ * @author illusi0n
+ *
+ */
+public class LunchOrderBean {
+
+	private String bread;
+	private String condiments;
+	private String dressing;
+	private String meat;
+
+	public LunchOrderBean() {
+	}
+
+	public String getBread() {
+		return bread;
+	}
+
+	public void setBread(String bread) {
+		this.bread = bread;
+	}
+
+	public String getCondiments() {
+		return condiments;
+	}
+
+	public void setCondiments(String condiments) {
+		this.condiments = condiments;
+	}
+
+	public String getDressing() {
+		return dressing;
+	}
+
+	public void setDressing(String dressing) {
+		this.dressing = dressing;
+	}
+
+	public String getMeat() {
+		return meat;
+	}
+
+	public void setMeat(String meat) {
+		this.meat = meat;
+	}
+}

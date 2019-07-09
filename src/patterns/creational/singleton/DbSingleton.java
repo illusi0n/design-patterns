a96d350c9707dcc81d07f037763cc6ef8355540b
illusi0n <miloshb92@hotmail.com>
2019-07-09 02:00:14 +0200

    Added thread safe

--- src/patterns/creational/singleton/DbSingleton.java
+++ src/patterns/creational/singleton/DbSingleton.java
@@ -2,14 +2,21 @@ package patterns.creational.singleton;
 
 public class DbSingleton {
 
-	private static DbSingleton instance = null;
+	private static volatile DbSingleton instance = null;
 
 	private DbSingleton() {
+		if (instance != null) {
+			throw new RuntimeException("Use getInstance() to get current instance");
+		}
 	};
 
 	public static DbSingleton getInstance() {
 		if (instance == null) {
-			instance = new DbSingleton();
+			synchronized(DbSingleton.class) {
+				if (instance == null) {
+					instance = new DbSingleton();					
+				}
+			}
 		}
 		return instance;
 	}
